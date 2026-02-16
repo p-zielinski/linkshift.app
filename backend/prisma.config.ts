6173efd14fa62873ac2f11d0a36c5d37dbbb5f68
p-zielinski <piotrzielinski96@yahoo.com>
2026-02-16 14:42:26 +0100

    fix for DATABASE_URL

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'prisma/config';
 import 'dotenv/config';
 
+console.log(process.env['' + 'DATABASE_URL']);
+
 export default defineConfig({
   datasource: {
     url: process.env['' + 'DATABASE_URL'],
