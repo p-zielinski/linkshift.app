8088daa230e42c979249200c35118f1a38cb435a
p-zielinski <piotrzielinski96@yahoo.com>
2026-02-16 08:19:15 +0100

    deploy test

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -1,8 +1,20 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  { path: 'dashboard', renderMode: RenderMode.Client },
+  { path: 'legal/consent', renderMode: RenderMode.Client },
+  { path: 'profile', renderMode: RenderMode.Client },
+  { path: 'organization', renderMode: RenderMode.Client },
+  { path: 'domains', renderMode: RenderMode.Client },
+  { path: 'domain-groups', renderMode: RenderMode.Client },
+  { path: 'redirect-rules', renderMode: RenderMode.Client },
+  { path: 'tests', renderMode: RenderMode.Client },
+  { path: 'verify-email', renderMode: RenderMode.Client },
+  { path: 'reset-password', renderMode: RenderMode.Client },
+  { path: 'invite', renderMode: RenderMode.Client },
+
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
