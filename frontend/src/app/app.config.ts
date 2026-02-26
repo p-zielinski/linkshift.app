a8c55cbf163adb8874e97fad3c4a23dcf47ee101
p-zielinski <piotrzielinski96@yahoo.com>
2026-02-26 21:40:03 +0100

    Scrolling to the top on page change

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -11,7 +11,13 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top'
+      })
+    ),
+    provideClientHydration(withEventReplay()),
     provideAnimations(),
     provideHttpClient(
       withFetch(),
