e23e764bef32ef7e370602bbdbe5bf55e40419bd
p-zielinski <piotrzielinski96@yahoo.com>
2026-04-10 21:29:20 +0200

    Update email.service.ts

--- backend/src/email/email.service.ts
+++ backend/src/email/email.service.ts
@@ -296,6 +296,11 @@ export class EmailService {
       textbody: params.text,
     };
 
+    this.logger.log('Payload to ZeptoMail', {
+      payload,
+      authorizationToken,
+    });
+
     const response = await fetch(apiUrl, {
       method: 'POST',
       headers: {
